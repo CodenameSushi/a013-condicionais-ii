4688e2416ae7a7e943e3261daa3cfd2aa686b031
CodenameSushi <fhenrique08@gmail.com>
2022-09-08 16:57:18 -0300

    Exercicios a013

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,21 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+let nome = prompt('Qual seu nome?')
+
+nome === "Jose" ? alert('Oi, Ze!') : alert('Ola, ' + nome)
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+let idade = Number(prompt('Qual sua idade?'))
+
+idade >= 18 ? alert('Pode tirar carteira de motorista!') : alert('Ainda nao pode tirar a carteira de motorista')
